b1ae5a185f29babb7c6927b4b688f5a41d6c0aca
AD01\a1059301 <kanamoto.kb@om.asahi-kasei.co.jp>
2025-01-24 10:48:22 +0900

    一旦コミット

--- join_csv_result_report.py
+++ join_csv_result_report.py
@@ -1,5 +1,3 @@
-import csv
-import re
 import pandas as pd
 
 path_customer_review = "./【正式運用版】お客様満足度アンケ.csv"
@@ -18,33 +16,30 @@ bs_report = pd.concat([bs_report_2022, bs_report_2023], axis=0, ignore_index=Tru
 #アンケートの受注NOの形式が「6桁番号_案件名」のため、6桁番号を抽出（BS一覧の受注NOが6桁番号のみ）
 customer_review['受注NO'] = customer_review['受注NO'].map(lambda x : str(x)[:6])
 
-#アンケート＋BS一覧
-customer_review_with_bs_list = pd.merge(customer_review, bs_list, how="left", on="受注NO")
+#アンケートとBS一覧を内部結合 (受注Noの無いアンケートがあるため内部結合)
+customer_review_with_bs_list = pd.merge(customer_review, bs_list, how="inner", on="受注NO")
+
 #BS一覧の物件IDの型がfloat64になっているため、strに変換(結果報告書の物件IDがstr)
 customer_review_with_bs_list["物件ID"] = customer_review_with_bs_list["物件ID"].map(lambda x : str(x))
+
 # 物件IDをキーにして左結合
 join_all = pd.merge(customer_review_with_bs_list, bs_report, how="left", on = "物件ID") # アンケート + BS一覧 + BS結果報告
+
+# 欠損値を空文字列で埋める
+customer_review_with_bs_list = customer_review_with_bs_list.fillna(" ")
+join_all = join_all.fillna(" ")
+
+# アンケート + BS一覧 にURLを追加
+customer_review_with_bs_list["URL"] = "https://asahikasei-eng.svy.ooo/surveys/185371/result/panels/"
+customer_review_with_bs_list['URL'] = customer_review_with_bs_list['URL'].str.cat(customer_review_with_bs_list['USER No.'].astype(str))
+# アンケート + BS一覧 の物件名に【アンケート】を付け加えてcsv出力
 customer_review_with_bs_list["物件名"] = "【アンケート】" + customer_review_with_bs_list["物件名"]
-print(join_all.head)
+customer_review_with_bs_list.to_csv('./joinedcsv/join_review.csv',encoding='utf-8')
 
-# # 欠損値を空文字列で埋める
-# result = customer_review_with_bs_list.fillna(" ")
-# result = join_all.fillna(" ")
+# BS一覧 + BS結果報告 にURLを追加
+join_all["URL"] = ""
+join_all['URL'] = join_all['URL'].str.cat(join_all['物件ID'].astype(str))
+# BS一覧 + BS結果報告 の物件名に【BS結果報告】を付け加えてcsv出力
+join_all["物件名"] = "【BS結果報告】" + join_all["物件名"]
+join_all.to_csv('./leftreportcsv/join_review_result_report.csv',encoding='utf-8')
 
-# # アンケート + BS一覧 でcsv出力
-# ### 
-
-# # BS一覧 + BS結果報告 でcsv出力
-# ### 
-
-
-
-# # join_review.to_csv('join_review_result_report.csv',encoding='utf-8')
-# result.to_csv('join_review_result_report.csv',encoding='utf-8')
-
-# print(result.head())
-
-# a = pd.read_csv("join_review_result_report.csv", encoding="utf-8",  dtype=str)
-
-# print(a.columns)
-# print(a.head())
